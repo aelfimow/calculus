0180eac260227f96942881452e3dc0a29acb2601
Alexander Elfimow <my.free.mail@gmx.de>
2018-06-03 22:05:32 +0200

    Added files

---
+++ Geometry/Equilateral_Triangle.m
@@ -0,0 +1,4 @@
+function area = Equilateral_Triangle(s)
+    % Compute area of the triangle
+    area = (sqrt(3) * s * s) / 4.0;
+end
---
+++ Geometry/Triangle.m
@@ -0,0 +1,4 @@
+function area = Triangle(b, h)
+    % Compute area of the triangle
+    area = (b * h) / 2.0;
+end
