806aeb3f536f9030a42109786a6e42e99ea8694b
Alexander Elfimow <my.free.mail@gmx.de>
2018-06-07 18:46:31 +0200

    Plot Circle: added parameters

--- Coordinate_Plane/Plot_Circle.m
+++ Coordinate_Plane/Plot_Circle.m
@@ -1,10 +1,23 @@
 1;
 
+% Circle parameters
+% Radius
+r = 1;
+
+% x and y offsets
+x_offset = 1;
+y_offset = 2;
+
+% Compute the points
 x = -r : 0.1 : r;
 
-y1 = sqrt(r^2 - x.^2);
-y2 = -y1;
+y = sqrt(r^2 - x.^2);
+y1 = y + y_offset;
+y2 = -y + y_offset;
 
+x += x_offset;
+
+% Plot it all
 plot(x, y1, x, y2);
 
 xlabel("x");
