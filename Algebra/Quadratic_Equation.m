c2e0258fc87794a7763679729859f24e5b02f5ef
Alexander Elfimow <my.free.mail@gmx.de>
2018-06-07 17:29:30 +0200

    Added directory

---
+++ Algebra/Quadratic_Equation.m
@@ -0,0 +1,12 @@
+function [x1, x2] = Quadratic_Equation(a, b, c)
+    D = b^2 - (4 * a * c);
+    if (D == 0)
+        % The equation has one real root
+        x1 = (-b + sqrt(D)) / (2 * a);
+        x2 = x1;
+    else
+        % The equation has two (real or complex) roots
+        x1 = (-b + sqrt(D)) / (2 * a);
+        x2 = (-b - sqrt(D)) / (2 * a);
+    endif
+end
